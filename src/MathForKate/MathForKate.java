955e2887fe2fe31d887a877b13cabf8a1b003cd1
mishok <misha_sha@list.ru>
2018-12-23 22:48:00 +0300

    вынес вычисления в отдельный класс

--- src/MathForKate/MathForKate.java
+++ src/MathForKate/MathForKate.java
@@ -4,9 +4,6 @@ import javax.swing.*;
 
 public class MathForKate {
 
-    private static int numOfTasks;
-    private static int minVal;
-    private static int maxVal;
     private JTextArea textMaxVal;
     private JTextField textFieldMaxVal;
     private JTextArea textMinVal;
@@ -17,116 +14,6 @@ public class MathForKate {
     private JCheckBox checkBoxPlusMinus;
     private JCheckBox checkBoxMinusPlus;
     private JButton buttonGenerate;
-
-    public void setNumOfTasks(int numOfTasks) {
-        this.numOfTasks = numOfTasks;
-    }
-
-    public void setMinVal(int minVal) {
-        this.minVal = minVal;
-    }
-
-    public void setMaxVal(int maxVal) {
-        this.maxVal = maxVal;
-    }
-
-
-    public static String plusTwo() {
-        int i = 1;
-        int x, y;
-        String s = "";
-        String xStr, yStr;
-
-        while (i <= numOfTasks) {
-            x = (int) (Math.random() * maxVal);
-            y = (int) (Math.random() * maxVal);
-            if (x + y > maxVal || x == 0 || y == 0) continue;
-            else {
-                if (x < 10) xStr = (x + " ");
-                else xStr = Integer.toString(x);
-                if (y < 10) yStr = (y + " ");
-                else yStr = Integer.toString(y);
-                s = s + (xStr + " + " + yStr + " = ____      ");
-                if (i % 3 == 0 && i != 0) s = s + "\n";
-                i++;
-            }
-        }
-        return (s + "\n");
-    }
-
-    public static String minusTwo() {
-        int i = 1;
-        int x, y;
-        String s = "";
-        String xStr, yStr;
-
-        while (i <= numOfTasks) {
-            x = (int) (Math.random() * maxVal);
-            y = (int) (Math.random() * maxVal);
-            if (x - y < minVal || x == 0 || y == 0) continue;
-            else {
-                if (x < 10) xStr = (x + " ");
-                else xStr = Integer.toString(x);
-                if (y < 10) yStr = (y + " ");
-                else yStr = Integer.toString(y);
-                s = s + (xStr + " - " + yStr + " = ____      ");
-                if (i % 3 == 0 && i != 0) s = s + "\n";
-                i++;
-            }
-        }
-        return (s + "\n");
-    }
-
-    public static String plusMinusThree() {
-        int i = 1;
-        int x, y, z;
-        String s = "";
-        String xStr, yStr, zStr;
-
-        while (i <= numOfTasks) {
-            x = (int) (Math.random() * maxVal);
-            y = (int) (Math.random() * maxVal);
-            z = (int) (Math.random() * maxVal);
-            if ((x + y - z) > maxVal || (x + y - z) < minVal || (x + y) > maxVal || x == 0 || y == 0 || z == 0) continue;
-            else {
-                if (x < 10) xStr = (x + " ");
-                else xStr = Integer.toString(x);
-                if (y < 10) yStr = (y + " ");
-                else yStr = Integer.toString(y);
-                if (z < 10) zStr = (z + " ");
-                else zStr = Integer.toString(z);
-                s = s + (xStr + " + " + yStr + " - " + zStr + " = ____      ");
-                if (i % 3 == 0 && i != 0) s = s + "\n";
-                i++;
-            }
-        }
-        return (s + "\n");
-    }
-    public static String minusPlusThree() {
-        int i = 1;
-        int x, y, z;
-        String s = "";
-        String xStr, yStr, zStr;
-
-        while (i <= numOfTasks) {
-            x = (int)(Math.random() * maxVal);
-            y = (int)(Math.random() * maxVal);
-            z = (int)(Math.random() * maxVal);
-            if ((x - y + z) > maxVal || (x - y + z) < minVal || (x - y) < minVal || x == 0 || y == 0 || z == 0) continue;
-            else {
-                if (x < 10) xStr = x + " ";
-                else xStr = Integer.toString(x);
-                if (y < 10) yStr = y + " ";
-                else yStr = Integer.toString(y);
-                if (z < 10) zStr = z + " ";
-                else zStr = Integer.toString(z);
-                s = s + (xStr + " - " + yStr + " + " + zStr + " = ____      ");
-                if (i % 3 == 0 && i != 0) s = s + "\n";
-                i++;
-            }
-        }
-        return (s + "\n");
-    }
 
 
     public static void main(String[] args) {
